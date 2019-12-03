9c349d77fa305554366f0d961c185fb7cb0ce964
ahmadelj <ahmad.eljachi98@gmail.com>
2019-12-03 10:51:13 +0100

    Added Sub method to the Math class

--- src/Math.java
+++ src/Math.java
@@ -6,15 +6,17 @@ public class Math {
     public static void main(String[] args) {
         Math myApp = new Math();
 
-        int a = 5;
+        int a = 7;
         int b = 6;
 
         int sum;
+        int subSum;
 
         sum = myApp.add(a, b);
+        subSum = myApp.sub(a, b);
 
         System.out.println(sum);
-
+        System.out.println(subSum);
 
 
 
@@ -24,5 +26,11 @@ public class Math {
         int sum;
         sum = first + second;
         return sum;
+    }
+
+    public int sub(int first, int second) {
+        int subSum;
+        subSum = first - second;
+        return subSum;
     }
 }
