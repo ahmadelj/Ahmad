8342721beeda3450ec95f5bb56c341e082ff54aa
ahmadelj <ahmad.eljachi98@gmail.com>
2019-12-03 10:41:02 +0100

    Added Add method to the Math class

--- src/Math.java
+++ src/Math.java
@@ -1,2 +1,28 @@
+
 public class Math {
+
+
+
+    public static void main(String[] args) {
+        Math myApp = new Math();
+
+        int a = 5;
+        int b = 6;
+
+        int sum;
+
+        sum = myApp.add(a, b);
+
+        System.out.println(sum);
+
+
+
+
+    }
+
+    public int add(int first, int second) {
+        int sum;
+        sum = first + second;
+        return sum;
+    }
 }
